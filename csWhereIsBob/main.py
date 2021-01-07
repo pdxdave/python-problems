359b122ee3fa6a7d380f0bb458a873a6386f6e4c
pdxdave <webdevdave@hotmail.com>
2021-01-07 06:22:56 -0800

    where is bob

--- csWhereIsBob/main.py
+++ csWhereIsBob/main.py
@@ -8,6 +8,17 @@ def csWhereIsBob(names):
             return key 
     return -1
 
+    # A second solution
+    # The range() function returns a sequence of numbers,
+    # starting from 0 by default, and increments
+    # by 1 (by default), and stops before a specified number.
+    # The len() function returns the numbers of the items in
+    # an object
+
+    for i in range(len(names)):
+        if names[i] == "Bob":
+            return i
+    return -1
 
 names = ["Kelly", "Pete", "Bob"]
 print(csWhereIsBob(names))
