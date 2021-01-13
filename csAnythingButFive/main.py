84b91c6f555c30976ea55870ae19c83847ce7e2e
pdxdave <webdevdave@hotmail.com>
2021-01-13 05:59:19 -0800

    another anything but five

--- csAnythingButFive/main.py
+++ csAnythingButFive/main.py
@@ -1,5 +1,6 @@
 # Given a start integer and an ending integer (both inclusive), write a function that returns the count (not the sum) of all the integers in the range (excpet integers that contain the digit 5)
 
+# 1
 def csAnythingButFive(start, end):
 
     count = 0
@@ -13,6 +14,19 @@ def csAnythingButFive(start, end):
             count += 1
     return count 
 
+start = 1
+end = 9
+print(csAnythingButFive(start, end))
+
+# 2
+def csAnythingButFive(start, end):
+    temp = [str(x) for x in range(start, end + 1)]
+    res = []
+    for i in temp:
+        if "5" not in i:
+            res.append(i)
+    return len(res)
+    
 start = 1
 end = 9
 print(csAnythingButFive(start, end))
