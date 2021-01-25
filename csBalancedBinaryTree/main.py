54ebcb98d58d518d28e10691aa8215ff71bf1bd3
pdxdave <webdevdave@hotmail.com>
2021-01-25 13:19:22 -0800

    balanced binary tree

---
+++ csBalancedBinaryTree/main.py
@@ -0,0 +1,43 @@
+#
+# Binary trees are already defined with this interface:
+# class Tree(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.left = None
+#     self.right = None
+def maxDepth(root):
+    '''
+    This will get the maxDepth height for both the left and right sides.
+    It will then forward them to the balancedBinaryTree method
+
+    First, we need to see if there is anything in the root.  If not, return 0
+    '''
+    if root is None:
+        return 0
+        
+    return max(maxDepth(root.left), maxDepth(root.right)) + 1
+    
+
+def balancedBinaryTree(root):
+    '''
+    First we need to see if there's anything in the root.  If not, then the tree would be
+    balanced at a 0 height
+    '''
+    if root is None:
+        return True 
+        
+    '''
+    Call the maxDepth method to get the maxDepth results for the left and right sides
+    '''
+    l = maxDepth(root.left)
+    r = maxDepth(root.right)
+
+    '''
+    We have three conditions to be met. 1) is the diff between the left and right less than or equal to one. 2)
+    is there a left root, and 3) is there a right root.  If all conditions are True, then the result is true
+    '''
+    
+    if ((l - r) <= 1) and balancedBinaryTree(root.left) is True and balancedBinaryTree(root.right) is True:
+        return True
+    
+    return False
