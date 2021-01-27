2f6d144c143cded1383ae609b98eebaa7788c135
pdxdave <webdevdave@hotmail.com>
2021-01-27 06:19:03 -0800

    fibonacci simple sum two

--- csFibonacciSimpleSum2/main.py
+++ csFibonacciSimpleSum2/main.py
@@ -1,3 +1,28 @@
+'''
+For a given positive integer n determine if it can be represented as a sum of two Fibonacci numbers (possibly equal).
+
+Example
+
+For n = 1, the output should be
+fibonacciSimpleSum2(n) = true.
+
+Explanation: 1 = 0 + 1 = F0 + F1.
+
+For n = 11, the output should be
+fibonacciSimpleSum2(n) = true.
+
+Explanation: 11 = 3 + 8 = F4 + F6.
+
+For n = 60, the output should be
+fibonacciSimpleSum2(n) = true.
+
+Explanation: 60 = 5 + 55 = F5 + F10.
+
+For n = 66, the output should be
+fibonacciSimpleSum2(n) = false.
+
+
+'''
 def fibonacciSimpleSum2(n):
     
     F = [0,1]
