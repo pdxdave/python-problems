99e4612b7559962d5840a97fb25b8d451c51e2e3
pdxdave <webdevdave@hotmail.com>
2021-01-27 06:07:38 -0800

    longest possible

--- csLongestPossible/main.py
+++ csLongestPossible/main.py
@@ -1,4 +1,11 @@
-# Given two strings that include only lowercase alpha characters, str_1 and str_2, write a function that returns a new sorted string that contains any character (only once) that appeared in str_1 or str_2
+'''
+Given two strings that include only lowercase alpha characters, str_1 and str_2, write a function that returns a new sorted string that contains any character (only once) that appeared in str_1 or str_2.
+
+Examples:
+
+csLongestPossible("aabbbcccdef", "xxyyzzz") -> "abcdefxyz"
+csLongestPossible("abc", "abc") -> "abc"
+'''
 
 def csLongestPossible(str_1, str_2):
 
