cfbdde6e4f61562ac24027a440cc6b678e8ebf74
pdxdave <webdevdave@hotmail.com>
2021-01-27 06:06:03 -0800

    sum of positive

--- csSumOfPostitive/main.py
+++ csSumOfPostitive/main.py
@@ -1,4 +1,12 @@
-# Given an array of integers, return the sum of all positive integers in an array
+'''
+Given an array of integers, return the sum of all the positive integers in the array.
+
+Examples:
+
+csSumOfPositive([1, 2, 3, -4, 5]) -> 1 + 2 + 3 + 5 = 11
+csSumOfPositive([-3, -2, -1, 0, 1]) -> 1
+csSumOfPositive([-3, -2]) -> 0
+'''
 
 def csSumOfPositive(input_arr):
 
