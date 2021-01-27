faaa937e562429b1a61d15e5fef5903b76f3066c
pdxdave <webdevdave@hotmail.com>
2021-01-27 06:04:36 -0800

    anything but five

--- csAnythingButFive/main.py
+++ csAnythingButFive/main.py
@@ -1,4 +1,12 @@
-# Given a start integer and an ending integer (both inclusive), write a function that returns the count (not the sum) of all the integers in the range (excpet integers that contain the digit 5)
+'''
+Given a start integer and an ending integer (both inclusive), write a function that returns the count (not the sum) of all integers in the range (except integers that contain the digit 5).
+
+Examples:
+
+csAnythingButFive(1, 5) -> 1, 2, 3, 4, -> 4 (there are 4 integers in the range that do not contain the digit 5)
+csAnythingButFive(1, 9) -> 1, 2, 3, 4, 6, 7, 8, 9 -> 8
+csAnythingButFive(4, 17) -> 4,6,7,8,9,10,11,12,13,14,16,17 -> 12
+'''
 
 # 1
 def csAnythingButFive(start, end):
