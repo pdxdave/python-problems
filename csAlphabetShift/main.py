ce50693ab1b08bce5bbb69e0123ed1f51fe3fbb9
pdxdave <webdevdave@hotmail.com>
2021-01-27 06:11:06 -0800

    alphabet shift

--- csAlphabetShift/main.py
+++ csAlphabetShift/main.py
@@ -1,6 +1,10 @@
-# Given a string, your task is to replace each of its
-# characters by the next one in the English alphabet, i.e. replace 'a' with 'b', replace 'b' with 'c'. 
-# 'z' would be replaced with 'a'
+'''
+Given a string, your task is to replace each of its characters by the next one in the English alphabet; i.e. replace a with b, replace b with c, etc (z would be replaced by a).
+
+Example
+
+For inputString = "crazy", the output should be alphabeticShift(inputString) = "dsbaz".
+'''
 
 def alphabetShift(inputString):
 
