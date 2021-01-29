460786fceb8dc16e1c68e7100f5fd91cbda11fba
pdxdave <webdevdave@hotmail.com>
2021-01-29 06:37:21 -0800

    find the single number

--- csFindTheSingleNumber/main.py
+++ csFindTheSingleNumber/main.py
@@ -1,3 +1,20 @@
+'''
+You are given a non-empty array of integers.
+
+One element appears exactly once, with every other element appearing at least twice, perhaps more.
+
+Write a function that can find and return the element that appears exactly once.
+
+Example 1:
+
+Input: [1,1,2,1]
+Output: 2
+Example 2:
+
+Input: [1,2,1,2,1,2,80]
+Output: 80
+'''
+
 def csFindTheSingleNumber(nums):
     res = {}
     for num in nums:
