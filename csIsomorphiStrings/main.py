c4f1aa1c4952c9c4498766c2879f0a99540ba373
pdxdave <webdevdave@hotmail.com>
2021-02-02 06:24:05 -0800

    isomorphic strings

--- csIsomorphiStrings/main.py
+++ csIsomorphiStrings/main.py
@@ -1,3 +1,28 @@
+'''
+Given two strings a and b, determine if they are isomorphic.
+
+Two strings are isomorphic if the characters in a can be replaced to get b.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character but a character may map to itself.
+
+Example 1:
+
+Input: 
+a = "odd"
+b = "egg"
+
+Output:
+true
+Example 2:
+
+Input:
+a = "foo"
+b = "bar"
+
+Output:
+false
+'''
+
 def csIsomorphicStrings(a, b):
     
     if len(a) != len(b):
