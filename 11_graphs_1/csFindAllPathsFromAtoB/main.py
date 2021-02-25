3e1449ec854d621f47ce6eed5bac89acacfe3d05
pdxdave <webdevdave@hotmail.com>
2021-02-24 18:04:33 -0800

    fix typo

--- 11_graphs_1/csFindAllPathsFromAtoB/main.py
+++ 11_graphs_1/csFindAllPathsFromAtoB/main.py
@@ -19,7 +19,7 @@ Example:
              
        index      0    1    2   3   4
 Input: graph = [[1, 2],[3],[3],[4],[]]
-       lenght     1     2   3   4   5
+       length     1     2   3   4   5
 Output: [[0,1,3,4], [0,2,3,4]]
 '''
 
