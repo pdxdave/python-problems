c95bdec807174b1697c8d4360a9829539d9776c7
pdxdave <webdevdave@hotmail.com>
2021-03-05 14:31:17 -0800

    make it jazzy

--- 01_python_basics/csMakeItJazzy/main.py
+++ 01_python_basics/csMakeItJazzy/main.py
@@ -1,4 +1,15 @@
-# Create a function that concatenates the number 7 to the end of every chord in the list.  If a chord already ends with a 7, ignore that chord
+""" 
+Create a function that concatenates the number 7 to the end 
+of every chord in the list.  If a chord already ends 
+with a 7, ignore that chord
+
+The append() method adds an item to the end of the list.
+
+append is twice as fast. In general case append will add 
+one item to the list, while += will copy all elements 
+of right-hand-side list into the left-hand-side list.
+
+"""
 
 def csMakeItJazzy(chords):
 
@@ -13,6 +24,27 @@ def csMakeItJazzy(chords):
             # append the chord with the number 7
             result += [chord + "7"]
 
+    return result
+
+chords = ["G", "F", "C", "Dm", "G7"]
+print(csMakeItJazzy(chords))
+
+
+# 2
+def csMakeItJazzy(chords):
+
+    # put the result in a list
+    result = []
+
+    # loop through the chords
+    for chord in chords:
+        # slice the last index off the string
+        if chord[-1] == "7":
+            # result += [chord]
+            result.append(chord)
+        else:
+            # result += [chord + "7"]
+            result.append(chord + "7")
     return result
 
 
