344a1ad60a797eb7afe52329ef87a897f70f5a31
pdxdave <webdevdave@hotmail.com>
2021-03-06 15:53:06 -0800

    dictionaries

--- 01_python_basics/demos/dictionaries.py
+++ 01_python_basics/demos/dictionaries.py
@@ -9,6 +9,8 @@ something = {
 
 Dictionary collections are unordered and changeable. 
 There are no duplicates
+To loop through a dictionary getting both keys and values, use .items(),
+otherwise .values()
 """
 # 1
 def dictTest(something):
@@ -44,8 +46,7 @@ print(dictTest(names))
 # 3
 """
 What if we were to take the same problem above, but return the names
-sorted.  Notice how they're returned in a list as tuples. This is because
-dictionaries are unordered.  To sort the data, they're returned as tuples. 
+sorted. We'll get a return of tuples b/c dictionaries are not sorted. 
 """
 
 def dictTest(names):
@@ -55,10 +56,58 @@ def dictTest(names):
     for i in range(len(names)):
         namesDict[names[i]] = names.count(names[i])
         
-    sorted_names = sorted(namesDict.items(), key=lambda kv: kv[0])
+    sorted_names = sorted(namesDict.items())
     
     return sorted_names
     
 names = ['sara', 'frank', 'peter','alen', 'kelly','frank', 'peter','sara', 'frank']
 print(dictTest(names))
 """[('alen', 1), ('frank', 3), ('kelly', 1), ('peter', 2), ('sara', 2)]"""
+
+# 4
+"""
+What if we were to take the same problem above, but return the names by value?
+Here we can use a lambda anonymous function. 
+"""
+
+def dictTest(names):
+    
+    namesDict = {}
+    
+    for i in range(len(names)):
+        namesDict[names[i]] = names.count(names[i])
+        
+    sorted_names = sorted(namesDict.items(), key=lambda kv: kv[1])
+    
+    return sorted_names
+    
+names = ['sara', 'frank', 'peter','alen', 'kelly','frank', 'peter','sara', 'frank']
+print(dictTest(names))
+"""[('alen', 1), ('kelly', 1), ('sara', 2), ('peter', 2), ('frank', 3)]"""
+
+
+#5 
+"""
+What if you have a list of dictionaries, but there are duplicates?  
+This is one way to remove them.
+"""
+
+def dictTest(names):
+    
+    seen = set()
+    result = []
+    for n in names:
+        get_items = tuple(n.items())
+        if get_items not in seen:
+            seen.add(get_items)
+            result.append(n)
+    
+    return result
+
+names = [
+    {'frank': 123},
+    {'steve': 456},
+    {'frank': 123},
+    {'kelly': 789}
+]
+print(dictTest(names))
